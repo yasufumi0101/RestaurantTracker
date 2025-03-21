781accaa76bb479ab11a6831879edfa8368fec1b
yasuno-N5j <yasuno@rokko-jitsugyo.co.jp>
2025-03-21 14:42:54 +0900

    DBテーブル作成

--- backend/models/restaurant.go
+++ backend/models/restaurant.go
@@ -1,7 +1,18 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Restaurant struct {
-	Name string  `JSON:"name"`
-	Lat  float64 `JSON:"lat"`
-	Lng  float64 `JSON:"lng"`
+	gorm.Model
+	UserID    uint    `gorm:"not null"`
+	Name      string  `gorm:"not null;unique"`
+	Lat       float64 `gorm:"not null"`
+	Lng       float64 `gorm:"not null"`
+	Rating    float64 `gorm:"not null"`
+	Memo      string
+	VisitedAt time.Time `gorm:"not null"`
 }
